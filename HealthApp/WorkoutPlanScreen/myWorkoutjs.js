e9927e96fd841a0ef67a41a7f76b58f077d9c2ce
Thomas Le <40319997+ThomasLe64@users.noreply.github.com>
2021-12-20 10:42:48 -0500

    Add files via upload

---
+++ HealthApp/WorkoutPlanScreen/myWorkoutjs.js
@@ -0,0 +1,45 @@
+var temp = localStorage.getItem(localStorage.getItem("day") + "list");
+var myList = JSON.parse(temp);
+
+
+
+//remove all checked elements
+function removeChecked(){
+    var list = document.querySelectorAll('input[type="checkbox"]:checked');
+    
+    for(var index = 0; index < list.length; index++){
+        var target = list[index].parentElement.textContent;
+        for(var t = 0; t < myList.length; t++){
+            if(target == myList[t]){
+                myList.splice(t,1);
+            }
+        }
+    }
+    localStorage[localStorage.getItem("day") + "list"] = JSON.stringify(myList);
+    viewList();
+}
+
+function viewList(){
+            //js to display workouts
+            var ul = document.getElementById("checklist");
+
+            ul.innerHTML = "";
+
+            for(var index = 0; index < myList.length; index++){
+    
+                var li = document.createElement("li");
+                li.innerHTML = myList[index];
+                var input = document.createElement("input");
+                input.type = "checkbox";
+
+                ul.appendChild(li);
+                li.appendChild(input);
+            }
+
+            var remain = document.getElementById("task-number");
+            remain.textContent = "You have " + myList.length + " workout remaining!";
+
+            if(myList.length == 0){
+                remain.textContent = "You have 0 workouts! please go back and click + to add more workouts.";
+            }
+}
